e3c565b0cfadd43893308198c2b9a508f6ed1e13
Oliver Gugger <gugger@puzzle.ch>
2019-06-21 22:56:14 +0200

    Fix OpenShift setup

--- build-and-push-docker.sh
+++ build-and-push-docker.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-./mvnw clean package -Pprod
+./mvnw clean package -Pprod -DskipTests
 
 cp src/main/docker/* target/
 
 docker build -t guggero/icedragon:latest target
 docker push guggero/icedragon:latest
+
+cd content-provider-demo-pages
+
+docker build -t guggero/icedragon-content-demo:latest .
+docker push guggero/icedragon-content-demo:latest
+
+cd ..
+
+oc import-image ice-dragon
+oc import-image ice-dragon-content-demo
